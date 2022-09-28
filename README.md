2c90c6e8f86ffa1fa378f4e1c226e42fe9499610
Hongyu Li <lihongyuim@163.com>
2022-09-28 16:36:40 +0800

    更新对项目目录的介绍

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 # decision-tree-2022
+
+## 项目目录
+
+```
+.
+├── README.md
+├── data
+│   └── 第1阶段.xlsx
+├── dataloader.py
+├── pipeline.py
+├── results
+├── test.ipynb
+├── test.py
+└── train.py
+```
+
+### data
+
+放置数据
+
+### result
+
+放置决策树模型结果，以及决策树形状图片
+
+### dataloader.py
+
+放置各类数据处理函数
+
+### pipeline.py
+
+模型的数据处理模块，按序列调用dataloader中的处理函数，每次处理可称为一个极端，上一阶段输出为下一阶段输入。
+
+示例：
+
+```python
+from dataloader import *
+
+def pipeline():
+    data = read_data()
+    data = womac_fillna(data)
+    data = process_string_clo(data)
+    data = womac_classification(data)
+    data = delete_nan_col_and_row(data)
+    data = precess_by_feature_meaning(data)
+    return get_train_data(data)
+```
+
+* 在`dataloader.py`中新增函数，并在`pipline.py`中调用以对数据进行进一步处理
+
+### train.py
+
+训练模块，简要介绍：
+
+1. 在其中调用`pipline`进行数据处理
+2. 在其中定义了`train`函数作为每次实验的训练函数
+3. 在其中修改超参数进行调参
+
+### test.ipynb
+
+快速实验
--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 
 from pipeline import pipeline
 
-def train(feature, label, feature_names, cls='entropy'):
+def train(feature, label, feature_names, id=0, cls='entropy'):
     x_train, x_test, y_train, y_test = train_test_split(feature, label, test_size=0.33, shuffle=True)
 
     clf = tree.DecisionTreeClassifier(criterion=cls)
@@ -13,7 +13,7 @@ def train(feature, label, feature_names, cls='entropy'):
     s = clf.score(x_test, y_test)
     print(f"accuracy: {s}")
 
-    with open("out.dot", 'w') as f :
+    with open(f"./results/out-{id}.dot", 'w') as f :
         f = tree.export_graphviz(clf, out_file=f,
                 feature_names=feature_names)
 
@@ -22,11 +22,11 @@ def train(feature, label, feature_names, cls='entropy'):
 
     import os  
     os.environ['PATH'] = os.pathsep + r'D:\tools\code\Graphviz\bin'
-    os.system('dot -Tpng out.dot -o 决策树模型.png')
+    os.system(f'dot -Tpng ./results/out-{id}.dot -o ./results/决策树模型-{id}-accuracy-{s:.3f}.png')
 
 epochs = 10
 feature, label, feature_name = pipeline()
 if __name__ == "__main__":
-    for i in range(epochs):
+    for epoch in range(epochs):
         # train(feature, label, feature_name, cls='gini')
-        train(feature, label, feature_name, cls='entropy')
+        train(feature, label, feature_name, id=epoch, cls='entropy')
