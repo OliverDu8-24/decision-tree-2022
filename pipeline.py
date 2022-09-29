f19b36eeb079b23917f040e1c000fa3f3bcf37b8
OliverDu <1550368479@qq.com>
2022-09-29 23:03:52 +0800

    加入相关性分析

--- pipeline.py
+++ pipeline.py
@@ -8,4 +8,6 @@ def pipeline():
     data = womac_classification(data)
     data = delete_nan_col_and_row(data)
     data = process_by_feature_meaning(data)
+    data = select_dominant_col(data)
+
     return get_train_data(data)
